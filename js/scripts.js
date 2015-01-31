f7659dc936f332834cee8f78b07252c0154e5c95
Eric Barrett <ericnbarrett@gmail.com>
2015-01-31 21:27:23 +0400

    tweaked design, header, footer

--- js/scripts.js
+++ js/scripts.js
@@ -6,19 +6,19 @@ L.tileLayer('http://ec2-54-76-157-122.eu-west-1.compute.amazonaws.com/open-ka/{z
 }).addTo(map);
 
 var marker01 = L.marker([41.714013, 44.783325]).addTo(map);
-marker01.bindPopup("<b>Hero Square</b>");
+marker01.bindPopup("<a href='loc2.html'>Hero Square, Area In front of the Circus Building Staircase</a>");
 
 var marker02 = L.marker([41.725919, 44.783589]).addTo(map);
-marker02.bindPopup("<b>Left Embankment</b>");
+marker02.bindPopup("<a href='loc3.html'>The Left Bank, The Slope of Vakhushti Bridge (on the side of Didube)</a>");
 
 var marker03 = L.marker([41.697818, 44.806881]).addTo(map);
-marker03.bindPopup("<b>Right Embankment</b>");
+marker03.bindPopup("<a href='loc1.html'>The Right Bank, The Slope of Nikoloz Baratashvili Bridge</a>");
 
 var marker04 = L.marker([41.704244, 44.790550]).addTo(map);
-marker04.bindPopup("<b>Rustaveli Avenue</b>");
+marker04.bindPopup("<a href='loc4.html'>Rustaveli, Rustaveli Ave./Elbakidze Slope Corner</a>");
 
 var marker05 = L.marker([41.727985, 44.768082]).addTo(map);
-marker05.bindPopup("<b>Saburtalo</b>");
+marker05.bindPopup("<a href='loc5.html'>Saburtalo, Pekini Ave./Vazha Pshavela Ave. Junction</a>");
 
 function toggleChevron(e) {
     $(e.target)
