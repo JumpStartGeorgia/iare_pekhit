4ccc22c9711b4fad008ae68b373932ed4ff10e19
Eric Barrett <ericnbarrett@gmail.com>
2015-02-03 18:15:33 +0400

    updated layouts, design

--- js/map_loc1.js
+++
@@ -1,9 +0,0 @@
-var site_map = L.map('map').setView([41.697818, 44.806881], 18);
-
-L.tileLayer('http://ec2-54-76-157-122.eu-west-1.compute.amazonaws.com/open-en/{z}/{x}/{y}.png', {
-  attribution: '',
-  maxZoom: 18
-}).addTo(site_map);
-
-var marker01 = L.marker([41.697818, 44.806881]).addTo(site_map);
-marker01.bindPopup("<b>Right Embankment, Slope of Nikoloz Baratashvili Bridge</b>");
--- js/map_loc3.js
+++
@@ -1,9 +0,0 @@
-var site_map = L.map('map').setView([41.725919, 44.783589], 18);
-
-L.tileLayer('http://ec2-54-76-157-122.eu-west-1.compute.amazonaws.com/open-en/{z}/{x}/{y}.png', {
-  attribution: '',
-  maxZoom: 18
-}).addTo(site_map);
-
-var marker01 = L.marker([41.725919, 44.783589]).addTo(site_map);
-marker01.bindPopup("<b>Left Embankment, Slope of Vakhushti Bridge (on the side of Didube)</b>");
